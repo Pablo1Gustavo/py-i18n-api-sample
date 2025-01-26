b8c17887b7589bdd7bba0a108b4092be4b436f25
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2025-01-26 10:20:43 -0300

    Add language selector with flask language apply middleware

--- app/fastapi/app.py
+++ app/fastapi/app.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI
+
+
+from typing import Callable
+from fastapi import FastAPI, Request
+
+from app.libs.translation.selector import LanguageSelector
+from app.libs.translation.fastapi.translator_response import TranslatorResponse
 
 from .colors_router import router as colors_router
 from .foods_router import router as foods_router
 from .house_router import router as house_router
 
-app = FastAPI()
+
+app = FastAPI(default_response_class=TranslatorResponse)
 
 app.include_router(colors_router)
 app.include_router(foods_router)
 app.include_router(house_router)
+    
+@app.middleware("http")
+async def apply_language(request: Request, call_next: Callable):
+    client_lang = LanguageSelector.get_from_header(request.headers.get("Accept-Language"))
+    LanguageSelector.set_language(client_lang)
+    return await call_next(request)
+
 
 @app.get("/test")
 async def test():
---
+++ app/libs/translation/selector.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from contextvars import ContextVar
+from enum import StrEnum
+
+class AppLanguage(StrEnum):
+    PT_BR = "pt-br"
+    JP = "jp"
+    EN = "en"
+
+    @classmethod
+    def from_str(cls, language: str):
+        for app_lang in cls:
+            if app_lang.value.startswith(language):
+                return app_lang
+        return cls.EN
+
+language_ctx = ContextVar("language", default=AppLanguage.EN)
+
+class LanguageSelector():
+    @staticmethod
+    def get_from_header(content: Optional[str]) -> AppLanguage:
+        if not content:
+            return AppLanguage.EN
+        content_lang = content.strip().lower().split(",")[0]
+        return AppLanguage.from_str(content_lang)
+    
+    @staticmethod
+    def set_language(language: AppLanguage) -> None:
+        language_ctx.set(language)
+    
+    @staticmethod
+    def get_language() -> AppLanguage:
+        return language_ctx.get()
