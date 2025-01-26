94b123a431d429f01e82bf64bd2f60393cc963fb
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2025-01-26 10:25:14 -0300

    Update initial translatable classes

--- app/libs/translation/text.py
+++ app/libs/translation/text.py
@@ -1,17 +1,22 @@
 from typing import Optional
-from .translator import global_translator
 
+from .selector import LanguageSelector
+
+# lazy translation (response time)
 class TranslatableStr(str):
     def __new__(cls, text: str, plural_form: Optional[str] = None):
         obj = super().__new__(cls, text)
         obj.plural_form = plural_form
+        obj.args = (),
+        obj.kwargs = {}
         return obj
     
     def format(self, *args, **kwargs) -> "TranslatableStr":
         self.args = args
         self.kwargs = kwargs
         return self
-    
+
+# eager translation (startup time)
 class StrWithTranslation(TranslatableStr):
     def __new__(
         cls,
@@ -19,8 +24,14 @@ class StrWithTranslation(TranslatableStr):
         plural_form: Optional[str] = None,
         *args,
         **kwargs
-    ):        
-        obj = super().__new__(cls, text, plural_form)
-        obj.format(*args, **kwargs)
+    ):
+        from .translator import Translator
 
-        return global_translator.translate(obj)
+        obj = super().__new__(cls, text, plural_form)
+        obj = obj.format(*args, **kwargs)
+
+        print(LanguageSelector.get_language())
+
+        translator = Translator(LanguageSelector.get_language())
+
+        return translator.translate(obj)
--- app/text_samples/colors.py
+++ app/text_samples/colors.py
@@ -1,42 +1,43 @@
 from dataclasses import dataclass
 
+from app.libs.translation.text import TranslatableStr
+
 @dataclass
 class RedColorException(Exception):
-    message: str = "Red is not a valid color"
-    description: str = "Red is a primary color"
+    message: str = TranslatableStr("Red is not a valid color")
+    description: str = TranslatableStr("Red is a primary color")
 
 class Color:
     def __init__(self, name: str, hex_code: str):
         self.name = name
         self.hex_code = hex_code
 
-color_item = Color("Pink", "#0000FF")
+color_item = Color(TranslatableStr("Pink"), "#0000FF")
 
 def get_colors() -> list[str]:
     return [
-        "Red",
-        "Green",
-        "Blue",
-        "Yellow",
-        "Orange",
-        "Purple",
+        TranslatableStr("Red"),
+        TranslatableStr("Blue"),
+        TranslatableStr("Yellow"),
+        TranslatableStr("Orange"),
+        TranslatableStr("Purple"),
     ]
 
 def dont_get_red(color: str) -> str:
-    if color == "Red":
+    if color.lower() == "red":
         raise RedColorException()
     return color
 
 def get_hex_to_colors_map() -> dict[str, str]:
     return {
-        "#FF0000": "Red",
-        "#00FF00": "Green",
-        "#0000FF": "Blue",
-        "#FFFF00": "Yellow",
-        "#FFA500": "Orange",
-        "#800080": "Purple",
+        "#FF0000": TranslatableStr("Red"),
+        "#00FF00": TranslatableStr("Green"),
+        "#0000FF": TranslatableStr("Blue"),
+        "#FFFF00": TranslatableStr("Yellow"),
+        "#FFA500": TranslatableStr("Orange"),
+        "#800080": TranslatableStr("Purple"),
     }
 
 def write_color_poem() -> str:
-    poem = "Rose are red, violets are blue..."
+    poem = TranslatableStr("Rose are red, violets are blue...")
     return poem
