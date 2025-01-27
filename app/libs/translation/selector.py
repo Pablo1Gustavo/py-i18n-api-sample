59c4efa9e0ad16c59314d57f35d4e53f94ceea27
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2025-01-27 20:44:24 -0300

    Change LanguageSelector to abstract class

--- app/libs/translation/selector.py
+++ app/libs/translation/selector.py
@@ -1,33 +1,46 @@
 from typing import Optional
 from contextvars import ContextVar
 from enum import StrEnum
+from abc import ABC, abstractmethod
+
 
 class AppLanguage(StrEnum):
     PT_BR = "pt-br"
     ES_AR = "es-ar"
+    EN_US = "en-us"
     JP = "jp"
-    EN = "en"
+
+    @staticmethod
+    def format_from_header(content: Optional[str]) -> str:
+        if not content:
+            return AppLanguage.EN.value
+        return content.strip().lower().split(",")[0]
 
     @classmethod
     def from_str(cls, language: str):
+        language = cls.format_from_header(language)
+
         for app_lang in cls:
             if app_lang.value == language:
                 return app_lang
         for app_lang in cls:
             if app_lang.value.startswith(language.split("-")[0]):
                 return app_lang
+            
         return cls.EN
+
+class LanguageSelector(ABC):
+    @staticmethod
+    @abstractmethod
+    def set_language(language: 'AppLanguage') -> None: ...
+
+    @staticmethod
+    @abstractmethod
+    def get_language() -> 'AppLanguage': ...
 
 language_ctx = ContextVar("language", default=AppLanguage.EN)
 
-class LanguageSelector():
-    @staticmethod
-    def get_from_header(content: Optional[str]) -> AppLanguage:
-        if not content:
-            return AppLanguage.EN
-        content_lang = content.strip().lower().split(",")[0]
-        return AppLanguage.from_str(content_lang)
-    
+class BaseLanguageSelector(LanguageSelector):    
     @staticmethod
     def set_language(language: AppLanguage) -> None:
         language_ctx.set(language)
