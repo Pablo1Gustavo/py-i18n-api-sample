7f791a6eb2a3c5a0dbec6022ee60d7546740ab82
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2025-01-28 10:26:10 -0300

    Add flask json provider

--- app/flask/app.py
+++ app/flask/app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, request
+
+from app.libs.translation.selector import AppLanguage
+from app.libs.translation.flask.flask_language_selector import FlaskLanguageSelector
+from app.libs.translation.flask.translator_json_provider import TranslatorJSONProvider
 
 from .colors_router import colors
 from .foods_router import foods
@@ -6,10 +10,18 @@ from .house_router import house
 
 
 app = Flask(__name__)
+app.json = TranslatorJSONProvider(app)
 
 app.register_blueprint(house)
 app.register_blueprint(foods)
 app.register_blueprint(colors)
+
+
+@app.before_request
+async def apply_language():
+    client_lang = AppLanguage.from_str(request.headers.get("Accept-Language"))
+    FlaskLanguageSelector.set_language(client_lang)
+
 
 @app.route('/', methods=['GET'])
 def test():
