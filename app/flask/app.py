7b06f6fb0e64dc37c003480b0c9f56edc60e3186
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2025-01-28 11:10:12 -0300

    Adjust way where flask apply translation

--- app/flask/app.py
+++ app/flask/app.py
@@ -2,20 +2,21 @@ from flask import Flask, request
 
 from app.libs.translation.selector import AppLanguage
 from app.libs.translation.flask.flask_language_selector import FlaskLanguageSelector
-from app.libs.translation.flask.translator_json_provider import TranslatorJSONProvider
+from app.libs.translation.translator import translate_return
 
 from .colors_router import colors
 from .foods_router import foods
 from .house_router import house
 
-
 app = Flask(__name__)
-app.json = TranslatorJSONProvider(app)
 
 app.register_blueprint(house)
 app.register_blueprint(foods)
 app.register_blueprint(colors)
 
+for rule in app.url_map.iter_rules():
+    endpoint = app.view_functions[rule.endpoint]
+    app.view_functions[rule.endpoint] = translate_return(FlaskLanguageSelector)(endpoint)
 
 @app.before_request
 async def apply_language():
